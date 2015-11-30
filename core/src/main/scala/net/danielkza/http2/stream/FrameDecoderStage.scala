6cb3b5affb60d7a2fad581865d98d8ac97b22f27
Daniel Miranda <danielkza2@gmail.com>
2015-11-30 12:48:14 -0200

    Update FrameDecoderStage preface handling

--- core/src/main/scala/net/danielkza/http2/stream/FrameDecoderStage.scala
+++ core/src/main/scala/net/danielkza/http2/stream/FrameDecoderStage.scala
@@ -2,19 +2,24 @@ package net.danielkza.http2.stream
 
 import akka.stream.stage._
 import akka.util.ByteString
-
-import net.danielkza.http2.protocol.{HTTP2Error, Frame}
+import net.danielkza.http2.util.Implicits._
+import net.danielkza.http2.protocol.Frame.Settings
+import net.danielkza.http2.protocol.Frame
 import net.danielkza.http2.protocol.coders.FrameCoder
 import net.danielkza.http2.stream.FrameDecoderStage.NotHTTP2Exception
 
-class FrameDecoderStage(val waitForPreface: Boolean) extends PushPullStage[ByteString, Frame] {
+import scala.annotation.tailrec
+import scalaz.{\/-, -\/}
+
+class FrameDecoderStage(val waitForClientPreface: Boolean) extends PushPullStage[ByteString, Frame] {
   import FrameDecoderStage.CONNECTION_PREFACE
 
   private val coder = new FrameCoder
   private var nextFrameCoder: Option[coder.DecodeState] = None
   private var stash = ByteString.empty
-  private var needed = if(!waitForPreface) -1 else CONNECTION_PREFACE.length
-  private var prefaceNeeded = !waitForPreface
+  private var needed = if(!waitForClientPreface) -1 else CONNECTION_PREFACE.length
+  private var clientPrefaceNeeded = waitForClientPreface
+  private var settingsFrameNeeded = !waitForClientPreface
 
   override def onPush(bytes: ByteString, ctx: Context[Frame]) = {
     stash ++= bytes
@@ -27,41 +32,50 @@ class FrameDecoderStage(val waitForPreface: Boolean) extends PushPullStage[ByteS
     if (stash.isEmpty) ctx.finish()
     else ctx.absorbTermination()
 
+  @tailrec
   private def run(ctx: Context[Frame]): SyncDirective = {
     if (needed == -1) {
       if (stash.length < Frame.HEADER_LENGTH) {
         pullOrFinish(ctx)
       } else {
-        coder.decodeHeader.run(stash).leftMap { error =>
-          ctx.fail(error.toException())
-        }.map { case (remaining, (nextFrameCoder, remainingFrameLen)) =>
-          stash = remaining
-          needed = remainingFrameLen
-          this.nextFrameCoder = Some(nextFrameCoder)
-          run(ctx)
-        }.merge
+        coder.decodeHeader.run(stash) match {
+          case -\/(error) =>
+            ctx.fail(error)
+          case \/-((remaining, (nextFrameCoder, remainingFrameLen))) =>
+            stash = remaining
+            needed = remainingFrameLen
+            this.nextFrameCoder = Some(nextFrameCoder)
+            run(ctx)
+        }
       }
     } else if (stash.length < needed) {
       pullOrFinish(ctx)
-    } else if(prefaceNeeded) {
+    } else if(clientPrefaceNeeded) {
       val (preface, rest) = stash.splitAt(needed)
       if(preface != CONNECTION_PREFACE)
         ctx.fail(NotHTTP2Exception())
       else {
-        prefaceNeeded = false
+        clientPrefaceNeeded = false
+        settingsFrameNeeded = true
         needed = -1
         stash = rest
-        pullOrFinish(ctx)
+        run(ctx)
       }
     } else {
-      nextFrameCoder.get.run(stash).leftMap { error =>
-        ctx.fail(error.toException())
-      }.map { case (remaining, frame) =>
-        stash = remaining
-        needed = -1
-        this.nextFrameCoder = None
-        ctx.push(frame)
-      }.merge
+      nextFrameCoder.get.run(stash) match {
+        case -\/(error) =>
+          ctx.fail(error)
+        case \/-((remaining, frame)) =>
+          if(settingsFrameNeeded && !frame.isInstanceOf[Settings])
+            ctx.fail(NotHTTP2Exception())
+          else {
+            settingsFrameNeeded = false
+            stash = remaining
+            needed = -1
+            this.nextFrameCoder = None
+            ctx.push(frame)
+          }
+      }
     }
   }
 
@@ -75,5 +89,5 @@ object FrameDecoderStage {
   case class NotHTTP2Exception(message: String = null, cause: Throwable = null)
     extends RuntimeException(message, cause)
 
-  final val CONNECTION_PREFACE = ByteString.fromString("PRI *\n   HTTP/2.0\\r\\n\\r\\nSM\\r\\n\\r\\n", "US-ASCII")
+  final val CONNECTION_PREFACE = hex_bs"505249202a20485454502f322e300d0a0d0a534d0d0a0d0a"
 }
